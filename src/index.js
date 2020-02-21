a6ffac9ef61942a0375ac829adf3790d47625047
Parfenovx1 <parfenovx1@gmail.com>
2020-02-21 02:30:15 +0200

    5commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// 1 ЗАДАНИЕ
+var i = 1;
+do{
+    console.log(i);
+    i++;
+}
+while((i % 2) != 0);
+
+
+// 2 ЗАДАНИЕ
+var input = prompt("Введите число:");
+var quantity = prompt("Введите количество чисел:");
+var min = 1;
+var max = 1000000;
+var current = 1;
+for(var i = min; i <= max; i++){
+    if(input % i == 0){
+        if(current++ <= quantity){
+                console.log(i);
+        }else{
+            break;
+        }
+}}
+
+
+// 3 ЗАДАНИЕ
+var number1 = Math.ceil(Math.random() * 10);
+var user;
+
+while(user !== number1)
+{
+    user = prompt("Угадайте число от 1 до 10");
+    if(user == number1)
+    {
+        console.clear();
+        console.log("Правильно!")
+        break;
+    }
+    if(user == null)
+    {
+        console.clear();
+        console.log("Вы отменили игру");
+        break;
+    }
+    console.log("Попробуйте еще раз");
+
+}
+
+
+
+
+
+
