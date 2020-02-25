55ed0a6c748783d7bc4c435a84a4a22a2f444b37
Parfenovx1 <parfenovx1@gmail.com>
2020-02-25 02:56:00 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-// 1 ЗАДАНИЕ 1 Способ
-var i = 1;
-do{
-    console.log(i);
-    i++;
-}
-while((i % 2) != 0);
+// // 1 ЗАДАНИЕ 1 Способ
+// var i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while((i % 2) != 0);
 
-// 1 ЗАДАНИЕ 2 Способ
-var i = 1;
-while((i % 2) != 0){
-    console.log(i);
-    i++;
-}
+// // 1 ЗАДАНИЕ 2 Способ
+// var i = 1;
+// while((i % 2) != 0){
+//     console.log(i);
+//     i++;
+// }
 
 
-// 2 ЗАДАНИЕ
-var input = prompt("Введите число:");
-var quantity = prompt("Введите количество чисел:");
-var min = 1;
-var max = 1000000;
-var current = 1;
-for(var i = min; i <= max; i++){
-    if(input % i == 0){
-        if(current++ <= quantity){
-                console.log(i);
-        }else{
-            break;
-        }
-}}
+// // 2 ЗАДАНИЕ
+// var input = prompt("Введите число:");
+// var quantity = prompt("Введите количество чисел:");
+// var min = 1;
+// var max = 1000000;
+// var current = 1;
+// for(var i = min; i <= max; i++){
+//     if(input % i == 0){
+//         if(current++ <= quantity){
+//                 console.log(i);
+//         }else{
+//             break;
+//         }
+// }}
 
 
 // 3 ЗАДАНИЕ
@@ -36,7 +36,7 @@ var user;
 
 while(user !== number1)
 {
-    user = prompt("Угадайте число от 1 до 10");
+    user = Number(prompt("Угадайте число от 1 до 10"));
     if(user == number1)
     {
         console.clear();
