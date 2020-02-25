1f5d59093424997df42ef8387a1b1a342fcc739c
Parfenovx1 <58718910+Parfenovx1@users.noreply.github.com>
2020-02-25 03:33:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-// // 1 ЗАДАНИЕ 1 Способ
-// var i = 1;
-// do{
-//     console.log(i);
-//     i++;
-// }
-// while((i % 2) != 0);
+// 1 ЗАДАНИЕ 1 Способ
+var i = 1;
+do{
+    console.log(i);
+    i++;
+}
+while((i % 2) != 0);
 
-// // 1 ЗАДАНИЕ 2 Способ
-// var i = 1;
-// while((i % 2) != 0){
-//     console.log(i);
-//     i++;
-// }
+// 1 ЗАДАНИЕ 2 Способ
+var i = 1;
+while((i % 2) != 0){
+    console.log(i);
+    i++;
+}
 
 
-// // 2 ЗАДАНИЕ
-// var input = prompt("Введите число:");
-// var quantity = prompt("Введите количество чисел:");
-// var min = 1;
-// var max = 1000000;
-// var current = 1;
-// for(var i = min; i <= max; i++){
-//     if(input % i == 0){
-//         if(current++ <= quantity){
-//                 console.log(i);
-//         }else{
-//             break;
-//         }
-// }}
+// 2 ЗАДАНИЕ
+var input = prompt("Введите число:");
+var quantity = prompt("Введите количество чисел:");
+var min = 1;
+var max = 1000000;
+var current = 1;
+for(var i = min; i <= max; i++){
+    if(input % i == 0){
+        if(current++ <= quantity){
+                console.log(i);
+        }else{
+            break;
+        }
+}}
 
 
 // 3 ЗАДАНИЕ
